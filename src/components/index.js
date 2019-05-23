c838db281b6f9b6298783cea30a1c721b67db75a
unknown <byambaa.122@gmail.com>
2019-05-23 15:13:44 +0800

    add base new button

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import BaseDialog from './BaseDialog'
 import BaseDialogDelete from './BaseDialogDelete'
+import BaseNewButton from './BaseNewButton'
 import BaseTable from './BaseTable'
 import MainBar from './MainBar'
 import MainDrawer from './MainDrawer'
@@ -7,6 +8,7 @@ import MainDrawer from './MainDrawer'
 export {
     BaseDialog,
     BaseDialogDelete,
+    BaseNewButton,
     BaseTable,
     MainBar,
     MainDrawer
